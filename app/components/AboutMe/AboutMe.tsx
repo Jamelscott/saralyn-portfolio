5d713df38b6331e7154d656d19c0a268fbed353d
Jamel-bbox <jamel@balloonbox.io>
2024-01-27 13:53:24 -0800

    commit

--- app/components/AboutMe/AboutMe.tsx
+++ app/components/AboutMe/AboutMe.tsx
@@ -7,8 +7,8 @@ function AboutMe() {
             <Image src={photo} alt='photo headshot'/>
             <div className='aboutMe-text-container'>
                 <h1 className='aboutMe-h1'>Hi, I‘m Sara </h1>
-                <p className='aboutMe-p'>I'm a passionate UX/UI designer dedicated to crafting seamless and visually captivating digital experiences.</p>
-                <p className='aboutMe-p'>My mission? To transform ideas into intuitive designs. Ready to elevate your digital presence? Let's create something extraordinary!</p>
+                <p className='aboutMe-p'>I&apos;m a passionate UX/UI designer dedicated to crafting seamless and visually captivating digital experiences.</p>
+                <p className='aboutMe-p'>My mission? To transform ideas into intuitive designs. Ready to elevate your digital presence? Let&apos;s create something extraordinary!</p>
             </div>
         </div>
      );
